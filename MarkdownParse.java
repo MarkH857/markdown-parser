3bd1ccfc1f8265186a1cc689a5bb1ce85314dd71
MarkH857 <97138169+MarkH857@users.noreply.github.com>
2022-04-15 17:22:16 -0700

    Update MarkdownParse.java
    Change the condition for the while loop into searching for whether there is closed bracket after the updated current index instead of using the length of the markdown file to evaluate.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,14 +11,13 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
+        while(markdown.indexOf("[", currentIndex) >= 0) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
-            System.out.println(currentIndex);
         }
 
         return toReturn;
