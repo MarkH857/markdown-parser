14ffe0420eddd0af2354143942de1c9759b5e02a
MarkH857 <97138169+MarkH857@users.noreply.github.com>
2022-04-22 17:04:24 -0700

    Add an additional testing method for getLinks

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,23 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.net.FileNameMap;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testGetLinks() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String contentOfTest = Files.readString(fileName);
+        assertEquals(MarkdownParse.getLinks(contentOfTest), 
+            List.of("https://something.com","some-thing.html"));
     }
 }
